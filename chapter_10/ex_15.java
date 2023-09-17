6e02cf5af8b7f32f6827e4a5f6573626023feeea
Woody <waiyanwoody@gmail.com>
2023-09-17 12:13:44 +0630

    update

--- chapter_10/ex_15.java
+++ chapter_10/ex_15.java
@@ -2,5 +2,5 @@ package chapter_10;
 
 public class ex_15 {
     // solution at AddressBook class 
-    // test ex_14 at TestAddressBook class 
+    // test ex_15 at TestAddressBook class 
 }
--- chapter_10/ex_16.java
+++ chapter_10/ex_16.java
@@ -2,5 +2,5 @@ package chapter_10;
 
 public class ex_16 {
     // solution at AddressBook class 
-    // test ex_14 at TestAddressBook class 
+    // test ex_16 at TestAddressBook class 
 }
